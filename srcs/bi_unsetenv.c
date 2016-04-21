52b89d731918ee039695ecf14d68370b34ea88b2
Jean-baptiste GUTHERTZ <jguthert@e3r4p12.42.fr>
2016-04-21 11:28:04 +0200

    unsetenv fixed

--- srcs/bi_unsetenv.c
+++ srcs/bi_unsetenv.c
@@ -6,7 +6,7 @@
 /*   By: jguthert <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/20 13:29:49 by jguthert          #+#    #+#             */
-/*   Updated: 2016/04/20 16:45:00 by jguthert         ###   ########.fr       */
+/*   Updated: 2016/04/21 11:26:58 by jguthert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,13 @@ static void unset_link(t_list **g_env, t_list *link)
 	prev = *g_env;
 	if (*g_env == link)
 	{
-		g_env = &prev->next;
-		ft_lstdelone(g_env, free_env);
+		*g_env = prev->next;
+		ft_lstdelone(&prev, free_env);
 		return ;
 	}
 	while (prev->next != link)
 		prev = prev->next;
-	prev = link->next;
+	prev->next = link->next;
 	ft_lstdelone(&link, free_env);
 }
 
@@ -46,5 +46,6 @@ int		bi_unsetenv(t_av *av, t_list **g_env, t_list **l_env)
 		}
 		link = link->next;
 	}
+	bi_env(av, g_env, l_env);
 	return (0);
 }
