add0ce5a2899fc2931687fe9167252bc44397241
Jean-baptiste GUTHERTZ <jguthert@e3r4p12.42.fr>
2016-04-24 18:57:02 +0200

    env WIP

--- srcs/bi_env.c
+++ srcs/bi_env.c
@@ -6,25 +6,61 @@
 /*   By: jguthert <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/16 19:53:06 by jguthert          #+#    #+#             */
-/*   Updated: 2016/04/24 17:02:03 by jguthert         ###   ########.fr       */
+/*   Updated: 2016/04/24 18:56:54 by jguthert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static void	print_env(t_list *env)
+{
+	while (env != NULL)
+	{
+		ft_putendl(((t_env *)env->content)->str);
+		env = env->next;
+	}
+}
+
+static void set_newarg(t_av av, t_list **n_env, t_list **l_env)
+{
+	char	*my_arg;
+	char	*my_cmd;
+	int		i;
+
+	i = 0;
+	while (av.arg[1][i] != '=' && av.arg[1][i] != '\0')
+		i++;
+	my_cmd = ft_strsub(av.arg[1], 0, i);
+	my_arg = ft_strchr(av.arg[1], '=');
+	bi_setenv(AV_INIT(my_cmd, my_arg, NULL, 1), n_env, l_env);
+	ft_strdel(&my_cmd);
+}
+
+static void	get_new_env(t_av av, t_list **g_env, t_list **l_env)
+{
+	t_list	*new_list;
+
+	new_list = *g_env;
+	set_newarg(av, &new_list, l_env);
+	return (new_list)
+}
+
 int			bi_env(t_av av, t_list **g_env, t_list **l_env)
 {
-	t_list	*c_env;
+	t_list	*n_env;
 
-	c_env = *g_env;
+	n_env = NULL;
 	if (av.argc != 0)
-		bi_setenv(av, &c_env, l_env);
+		n_env = get_new_env(av, g_env, l_env);
 	if (av.argc > 1)
-		do_exec(av, c_env, *l_env);
-	while (c_env != NULL)
+		do_exec(av, n_env, *l_env);
+	else if (av.argc == 1)
 	{
-		ft_putendl(((t_env *)c_env->content)->str);
-		c_env = c_env->next;
+		print_env(n_env);
+		if (n_env != NULL)
+			ft_lstdel(&n_env, free_env);
 	}
+	else
+		print_env(*g_env);
 	return (0);
 }
