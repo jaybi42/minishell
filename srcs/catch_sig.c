dac929395fdb684193520e2f38f579b692c7ba8d
Jean-baptiste GUTHERTZ <jguthert@e2r9p4.42.fr>
2016-05-11 14:23:23 +0200

    sig

--- srcs/catch_sig.c
+++ srcs/catch_sig.c
@@ -6,7 +6,7 @@
 /*   By: jguthert <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/11 12:59:02 by jguthert          #+#    #+#             */
-/*   Updated: 2016/05/11 14:13:07 by jguthert         ###   ########.fr       */
+/*   Updated: 2016/05/11 14:19:36 by jguthert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 static void     is_sig(int signum)
 {
     if (signum == SIGINT)
-		ft_putstr_fd("\n", 2);
+		ft_putstr_fd("\n", 0);
 }
 
 void			catch_sig()
