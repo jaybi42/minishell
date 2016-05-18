e4fe0fd571b39d88ce19d635a63289cc714a6405
Jean-baptiste GUTHERTZ <jguthert@e3r4p16.42.fr>
2016-05-18 21:39:21 +0200

    signals fixed project ended

--- srcs/catch_sig.c
+++ srcs/catch_sig.c
@@ -6,7 +6,7 @@
 /*   By: jguthert <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/11 12:59:02 by jguthert          #+#    #+#             */
-/*   Updated: 2016/05/12 12:53:49 by jguthert         ###   ########.fr       */
+/*   Updated: 2016/05/18 21:26:55 by jguthert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,17 @@ static t_prompt	g_prompt = {0, NULL, NULL};
 
 static void		is_sig(int signum)
 {
-	if (signum == SIGINT)
-	{
-		ft_putchar('\n');
-		print_prompt(g_prompt.rand, g_prompt.g_env, g_prompt.l_env);
-	}
+	ft_putchar('\n');
+	ft_putstr("signal: ");
+	ft_putnbr(signum);
+	ft_putendl(" received.");
+	print_prompt(g_prompt.rand, g_prompt.g_env, g_prompt.l_env);
 }
 
 void			catch_sig(int rand, t_list *g_env, t_list *l_env)
 {
 	g_prompt = (t_prompt){rand, g_env, l_env};
 	signal(SIGINT, is_sig);
-	signal(SIGSTOP, SIG_IGN);
+	signal(SIGTSTP, is_sig);
+	signal(SIGQUIT, is_sig);
 }
