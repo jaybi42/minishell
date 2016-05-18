df87ee490bea5802b3cca214265bfc1408927ee4
Jean-baptiste GUTHERTZ <jguthert@e3r4p16.42.fr>
2016-05-18 19:53:29 +0200

    .h norme

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: jguthert <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/08 17:56:10 by jguthert          #+#    #+#             */
-/*   Updated: 2016/05/12 12:26:29 by jguthert         ###   ########.fr       */
+/*   Updated: 2016/05/18 19:53:21 by jguthert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,13 @@
 ** TODO: Suppr stdio
 */
 
-# include <stdio.h> //
-
 # include "libft.h"
 
 # define INIT_ALL(CMD,AV1,AV2) (char *[]){CMD, AV1, AV2, NULL}
 # define INIT_ARG(AV1,AV2) (char *[]){AV1, AV2, NULL}
 # define INIT_AV(A,B,C,D) ((t_av){A, INIT_ARG(B, C), D, INIT_ALL(A, B, C)})
 
-typedef void	(*sig_t) (int);
+typedef void	(*t_sig) (int);
 
 typedef struct	s_env
 {
